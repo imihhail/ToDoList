d5d9586ec95883cf4f88483e0e0374c31a39f48e
The-Estonian <Zaar2213@gmail.com>
2024-05-31 20:41:47 +0300

    created helper and manu files

--- todomvc/src/index.js
+++ todomvc/src/index.js
@@ -88,26 +88,3 @@ fw.Render({
   attri: ['id', 'hello1'],
 });
 
-// // route button that switches all Content data to home1
-// fw.Route({
-//   parent: 'Menu',
-//   element: 'button',
-//   styleClass: 'buttom',
-//   content: 'Home',
-//   attri: ['id', 'home'],
-//   where: '/home',
-//   contentParent: 'Content',
-//   contentToAdd: 'home1',
-// });
-
-// // route button that switches all Content data to hello1
-// fw.Route({
-//   parent: 'Menu',
-//   element: 'button',
-//   styleClass: 'buttom',
-//   content: 'Hello',
-//   attri: ['id', 'hello'],
-//   where: '/hellohello',
-//   contentParent: 'Content',
-//   contentToAdd: 'hello1',
-// });
